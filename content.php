6f53fcbf3168c1ede87ac863020ae80477fbb4b0
Tina Bell Vance <tvance@ag.com>
2018-03-17 17:32:10 -0400

    Adding more markup, styles

--- content.php
+++ content.php
@@ -3,4 +3,5 @@
 	<p class="post-meta"><?php the_date(); ?> <a href="#"><?php the_author(); ?></a></p>
 
 	<?php the_content( $more_link_text = null, $strip_teaser = false ); ?>
+    <span class="post-end">&#9884;</span>
 </div><!-- /.post -->
